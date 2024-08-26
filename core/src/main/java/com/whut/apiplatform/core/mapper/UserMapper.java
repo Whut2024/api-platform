c46fd41f333bb83ccd09cdc4ef7792d8bcaa28b4
Whut2024 <349953@whut.edu.cn>
2024-08-26 09:56:37 +0800

    move mapper

--- core/src/main/java/com/whut/apiplatform/core/mapper/UserMapper.java
+++ core/src/main/java/com/whut/apiplatform/core/mapper/UserMapper.java
@@ -1,4 +1,4 @@
-package com.whut.apiplatform.mapper;
+package com.whut.apiplatform.core.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.whut.apiplatform.model.entity.User;
@@ -6,7 +6,7 @@ import com.whut.apiplatform.model.entity.User;
 /**
 * @author laowang
 * @description 针对表【user(用户表)】的数据库操作Mapper
-* @createDate 2024-08-24 20:59:37
+* @createDate 2024-08-25 20:56:43
 * @Entity xx.domain.User
 */
 public interface UserMapper extends BaseMapper<User> {
